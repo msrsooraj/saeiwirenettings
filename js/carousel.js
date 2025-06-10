4ffa1dc804e2ab68bcd5dc8db9235e85d02d724e
unknown <msrsooraj@hotmail.com>
2025-06-10 13:53:26 +0530

    images to background

--- js/carousel.js
+++ js/carousel.js
@@ -1,24 +1,22 @@
-/* js/carousel.js */
-const track = document.querySelector('.carousel-track');
-const slides = Array.from(track.children);
-const nextButton = document.querySelector('.next');
-const prevButton = document.querySelector('.prev');
-let currentSlide = 0;
+const background = document.querySelector('.background-carousel');
 
-function updateSlidePosition() {
-  const slideWidth = slides[0].getBoundingClientRect().width;
-  track.style.transform = `translateX(-${slideWidth * currentSlide}px)`;
+const images = [
+  'images/image1.jpeg',
+  'images/image2.jpeg',
+  'images/image3.jpeg',
+  'images/image4.jpeg',
+  'images/image5.jpeg',
+  'images/image6.jpeg'
+];
+
+let index = 0;
+
+function changeBackground() {
+  background.style.backgroundImage = `url('${images[index]}')`;
+  index = (index + 1) % images.length;
 }
 
-nextButton.addEventListener('click', () => {
-  currentSlide = (currentSlide + 1) % slides.length;
-  updateSlidePosition();
+window.addEventListener('load', () => {
+  changeBackground();
+  setInterval(changeBackground, 30000); // Change every 30 seconds
 });
-
-prevButton.addEventListener('click', () => {
-  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
-  updateSlidePosition();
-});
-
-window.addEventListener('resize', updateSlidePosition);
-window.addEventListener('load', updateSlidePosition);
